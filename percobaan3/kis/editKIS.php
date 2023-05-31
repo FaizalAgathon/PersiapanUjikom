d624b9e79bca707f37cab86e435ba3af384006be
ciloks <ajar9378@gmail.com>
2023-05-31 10:35:13 +0700

    editkis

--- percobaan3/kis/editKIS.php
+++ percobaan3/kis/editKIS.php
@@ -10,14 +10,30 @@
 
 <?php include "../koneksi.php";
 
-if (isset($_POST['editKis'])) {
-  mysqli_query($link, "UPDATE kis SET 
-    nikWarga = '$_POST[warga]',
-    idFaskes = $_POST[faskes]
-    WHERE noKIS = '$_POST[editKIS]'
-  ");
+// if (isset($_POST['editKis'])) {
+//   mysqli_query($link, "UPDATE kis SET 
+//     nikWarga = '$_POST[warga]',
+//     idFaskes = '$_POST[faskes]'
+//     WHERE noKIS = '$_POST[editKIS]'
+//   ");
 
+//   header("Location: kis.php");
+// }
+
+
+// Proses pengeditan data
+if (isset($_POST['editKis'])) {
+  $nikWarga = $_POST['warga'];
+  $idFaskes = $_POST['faskes'];
+  $idkis = $_GET['id'];
+
+  // Perintah UPDATE untuk mengedit data
+  $query = "UPDATE kis SET nikWarga = '$nikWarga', idFaskes = '$idFaskes' WHERE noKIS = '$idkis'";
+  mysqli_query($link, $query);
+
+  // Redirect atau arahkan ke halaman setelah pengeditan data
   header("Location: kis.php");
+  exit;
 }
 
 $kis = query("SELECT * FROM kis WHERE noKIS = $_GET[id]")[0];
@@ -33,6 +49,7 @@ $kis = query("SELECT * FROM kis WHERE noKIS = $_GET[id]")[0];
         </option>
       <?php endforeach ?>
     </select> <br>
+
     Faskes : <select name="faskes">
       <?php foreach (query("SELECT * FROM faskes") as $faskes) : ?>
         <option value="<?= $faskes['idFaskes'] ?>" 

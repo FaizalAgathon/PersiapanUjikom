29152b7df369602b99c6e3133ea8628def37d0a1
benno25 <benno3811@gmail.com>
2023-05-31 11:05:04 +0700

    kontol

--- percobaan3/home.php
+++ percobaan3/home.php
@@ -15,6 +15,7 @@
   <a href="faskes/faskes.php">Daftar Faskes</a>
   <a href="warga/warga.php">Daftar Warga</a>
   <a href="ubahPassword.php">Ubah Password</a>
+  <a href="logout.php">Keluar</a>
 </body>
 
 </html>
